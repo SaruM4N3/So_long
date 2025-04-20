5ffc4abc4679afb4e2984234e6c91fb05f455451
SaruM4N3 <zakobajo@gmail.com>
2025-04-20 14:09:27 +0200

    add cleanrender function to render.c

--- src/render.c
+++ src/render.c
@@ -6,7 +6,7 @@
 /*   By: zsonie <zsonie@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/20 13:49:39 by zsonie            #+#    #+#             */
-/*   Updated: 2025/04/20 13:57:45 by zsonie           ###   ########.fr       */
+/*   Updated: 2025/04/20 14:04:39 by zsonie           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,4 +31,34 @@ void	init_textures(t_gameenv *env)
 			env->img->img.collect, &(env->img->img.width), &(env->img->img.height));
 	env->img->img.img->img_player = mlx_xpm_file_to_image(env->mlx_ptr,
 			env->img->img.player, &(env->img->img.width), &(env->img->img.height));
+}
+
+void    render_textures(t_gameenv env)
+{
+    
+}
+
+int	clean_render(t_gameenv *env)
+{
+	int		i;
+
+	i = 0;
+	if (env->grid != NULL)
+	{
+		while (env->grid[i] != NULL)
+		{
+			free(env->grid[i]);
+			i++;
+		}
+		free(env->grid);
+		mlx_destroy_image(env->mlx_ptr, env->img->img.img_wall);
+		mlx_destroy_image(env->mlx_ptr, env->img->img.img_ground);
+		mlx_destroy_image(env->mlx_ptr, env->img->img.img_coin);
+		mlx_destroy_image(env->mlx_ptr, env->img->img.img_player);
+		mlx_destroy_image(env->mlx_ptr, env->img->img.img_exit);
+		mlx_destroy_window(env->mlx_ptr, env->mlx_win);
+	}
+	mlx_destroy_display(env->mlx_ptr);
+	free(env->mlx_ptr);
+	exit(0);
 }
